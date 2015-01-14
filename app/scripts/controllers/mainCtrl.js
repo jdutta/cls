57ab5c5b471a0fcef20acdfa53e1a71bb00b0316
Joy Dutta <joy@elastica.co>
2015-01-14 12:47:08 -0800

    Added ability to specify search type

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -8,15 +8,15 @@
 angular.module('clsApp').controller('MainCtrl', function ($scope, $sce) {
     'use strict';
 
-    function getLocations(locations, query) {
+    function getLocations(locations, searchType, query) {
         var locArr = locations.replace(/ /g, '').split(',');
         return locArr.map(function (loc) {
-            return $sce.trustAsResourceUrl('http://' + loc + '.craigslist.org/search/' + query);
+            return $sce.trustAsResourceUrl('http://' + loc + '.craigslist.org/search/' + searchType + '?' + query);
         });
     }
 
     function computeUrlsArr() {
-        $scope.model.urlsArr = getLocations($scope.model.locations, $scope.model.query);
+        $scope.model.urlsArr = getLocations($scope.model.locations, $scope.model.searchType, $scope.model.query);
     }
 
     function onTyping(evt) {
@@ -28,7 +28,8 @@ angular.module('clsApp').controller('MainCtrl', function ($scope, $sce) {
     $scope.model = {
         locations: 'sfbay,losangeles,phoenix,dallas,denver',
         urlsArr: [],
-        query: 'mcy?query=r1200gs'
+        searchType: 'mcy',
+        query: 'query=r1200gs'
     };
 
     $scope.onTyping = onTyping;
