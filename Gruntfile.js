8e3a36fb9e116a91428d4161fb6caa1713638225
Joy Dutta <therider@gmail.com>
2015-09-02 20:38:58 -0700

    Removed dist/ from master.

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,8 +373,8 @@ module.exports = function (grunt) {
       ],
       dist: [
         'compass:dist',
-        'imagemin',
-        'svgmin'
+        //'imagemin',
+        //'svgmin'
       ]
     },
 
